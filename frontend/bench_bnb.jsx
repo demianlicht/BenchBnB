1c53425f2d0830e8b9f3d346235182c289ee3534
gettinToasty <sean.r.beyer@gmail.com>
2017-01-06 09:25:03 -0800

    Setup React framework

---
+++ frontend/bench_bnb.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+    ReactDOM.render(
+      <h1>Welcome to BenchBnB</h1>,
+      document.getElementById('root')
+    );
+});
